90463796adac7e549d745d9d32b806b84dd577c4
toastermagic <toastermagic@gmail.com>
2016-07-03 00:07:43 +0100

    made tweetSocket a window object

--- app/scripts/sockets.js
+++ app/scripts/sockets.js
@@ -1,41 +1,46 @@
-var socket = io();
+window.tweetSocket = (function (document) {
+    'use strict';
+    var socket = io();
 
-var mySocket = {
-    tweetStream: () => {
-        let streamSource = Rx.Observable.create((observer) => {
-            socket.on("tweet", (tweet) => {
-                observer.onNext(tweet);
-            });
-        });
-
-        return streamSource.pausable();
-    },
-    statusStream: Rx.Observable.create((observer) => {
-        socket.on("trainingStatus", (status) => {
-            observer.onNext({
-                training: status.status === "RUNNING",
-                message: status.message
-            });
-        });
-    }),
-    requestTweets: (afterTweetId, limit) => {
-        return Rx.Observable.create((observer) => {
-            socket
-                .emit("tweetRequest", {
-                    afterTweetId,
-                    limit
-                }, (tweets) => {
-                    observer.onNext(tweets);
+    var mySocket = {
+        tweetStream: () => {
+            let streamSource = Rx.Observable.create((observer) => {
+                socket.on("tweet", (tweet) => {
+                    observer.onNext(tweet);
                 });
-        });
-    },
-    classifyTweet: (options) => {
-        socket.emit("classify", {
-            tweetId: options.tweetId,
-            class: options.classification
-        });
-    },
-    sendCommand: (commandName, options) => {
-        socket.emit(commandName, options);
-    }
-};
+            });
+
+            return streamSource.pausable();
+        },
+        statusStream: Rx.Observable.create((observer) => {
+            socket.on("trainingStatus", (status) => {
+                observer.onNext({
+                    training: status.status === "RUNNING",
+                    message: status.message
+                });
+            });
+        }),
+        requestTweets: (afterTweetId, limit) => {
+            return Rx.Observable.create((observer) => {
+                socket
+                    .emit("tweetRequest", {
+                        afterTweetId,
+                        limit
+                    }, (tweets) => {
+                        observer.onNext(tweets);
+                    });
+            });
+        },
+        classifyTweet: (options) => {
+            socket.emit("classify", {
+                tweetId: options.tweetId,
+                class: options.classification
+            });
+        },
+        sendCommand: (commandName, options) => {
+            socket.emit(commandName, options);
+        }
+    };
+
+    return mySocket;
+})(document);

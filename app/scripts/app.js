71f6a833aeb7594fa80be6695a377647868b8cd8
toastermagic <toastermagic@gmail.com>
2016-07-04 16:48:42 +0100

    linter appeasement

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,85 +10,85 @@ subject to an additional IP rights grant found at
 http://polymer.github.io/PATENTS.txt
 */
 
-(function(document) {
-  'use strict';
+(function (document) {
+	"use strict";
 
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
+	// Grab a reference to our auto-binding template
+	// and give it some initial binding values
+	// Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+	var app = document.querySelector("#app");
 
-  app.showProgress = false;
+	app.showProgress = false;
 
-  app.shuffle = function() {
-    app.$.tweetList._shuffle();
-  };
+	app.shuffle = function () {
+		app.$.tweetList._shuffle();
+	};
 
-  app.add = function() {
-    app.$.tweetList._add();
-  };
+	app.add = function () {
+		app.$.tweetList._add();
+	};
 
-  // Sets app default base URL
-  app.baseUrl = '/';
-  if (window.location.port === '') {  // if production
-    // Uncomment app.baseURL below and
-    // set app.baseURL to '/your-pathname/' if running from folder in production
-    // app.baseUrl = '/polymer-starter-kit/';
-  }
+	// Sets app default base URL
+	app.baseUrl = "/";
+	if (window.location.port === "") {  // if production
+		// Uncomment app.baseURL below and
+		// set app.baseURL to "/your-pathname/" if running from folder in production
+		// app.baseUrl = "/polymer-starter-kit/";
+	}
 
-  app.displayInstalledToast = function() {
-    // Check to make sure caching is actually enabled—it won't be in the dev
-    // environment.
-    if (!Polymer.dom(document).querySelector('platinum-sw-cache').disabled) {
-      Polymer.dom(document).querySelector('#caching-complete').show();
-    }
-  };
+	app.displayInstalledToast = function () {
+		// Check to make sure caching is actually enabled—it won"t be in the dev
+		// environment.
+		if (!Polymer.dom(document).querySelector("platinum-sw-cache").disabled) {
+			Polymer.dom(document).querySelector("#caching-complete").show();
+		}
+	};
 
-  // Listen for template bound event to know when bindings
-  // have resolved and content has been stamped to the page
-  app.addEventListener('dom-change', function() {});
+	// Listen for template bound event to know when bindings
+	// have resolved and content has been stamped to the page
+	app.addEventListener("dom-change", function () { });
 
-  // See https://github.com/Polymer/polymer/issues/1381
-  window.addEventListener('WebComponentsReady', function() {
-    // imports are loaded and elements have been registered
-  });
+	// See https://github.com/Polymer/polymer/issues/1381
+	window.addEventListener("WebComponentsReady", function () {
+		// imports are loaded and elements have been registered
+	});
 
-  // Main area's paper-scroll-header-panel custom condensing transformation of
-  // the appName in the middle-container and the bottom title in the
-  // bottom-container.
-  // The appName is moved to top and shrunk on condensing. The bottom sub title
-  // is shrunk to nothing on condensing.
-  window.addEventListener('paper-header-transform', function(e) {
-    var appName = Polymer.dom(document).querySelector('#mainToolbar .app-name');
-    var middleContainer =
-        Polymer.dom(document).querySelector('#mainToolbar .middle-container');
-    var bottomContainer =
-        Polymer.dom(document).querySelector('#mainToolbar .bottom-container');
-    var detail = e.detail;
-    var heightDiff = detail.height - detail.condensedHeight;
-    var yRatio = Math.min(1, detail.y / heightDiff);
-    // appName max size when condensed. The smaller the number the smaller the
-    // condensed size.
-    var maxMiddleScale = 0.50;
-    var auxHeight = heightDiff - detail.y;
-    var auxScale = heightDiff / (1 - maxMiddleScale);
-    var scaleMiddle =
-        Math.max(maxMiddleScale, auxHeight / auxScale + maxMiddleScale);
-    var scaleBottom = 1 - yRatio;
+	// Main area"s paper-scroll-header-panel custom condensing transformation of
+	// the appName in the middle-container and the bottom title in the
+	// bottom-container.
+	// The appName is moved to top and shrunk on condensing. The bottom sub title
+	// is shrunk to nothing on condensing.
+	window.addEventListener("paper-header-transform", function (e) {
+		var appName = Polymer.dom(document).querySelector("#mainToolbar .app-name");
+		var middleContainer =
+			Polymer.dom(document).querySelector("#mainToolbar .middle-container");
+		var bottomContainer =
+			Polymer.dom(document).querySelector("#mainToolbar .bottom-container");
+		var detail = e.detail;
+		var heightDiff = detail.height - detail.condensedHeight;
+		var yRatio = Math.min(1, detail.y / heightDiff);
+		// appName max size when condensed. The smaller the number the smaller the
+		// condensed size.
+		var maxMiddleScale = 0.50;
+		var auxHeight = heightDiff - detail.y;
+		var auxScale = heightDiff / (1 - maxMiddleScale);
+		var scaleMiddle =
+			Math.max(maxMiddleScale, auxHeight / auxScale + maxMiddleScale);
+		var scaleBottom = 1 - yRatio;
 
-    // Move/translate middleContainer
-    Polymer.Base.transform('translate3d(0,' + yRatio * 100 + '%,0)',
-                           middleContainer);
+		// Move/translate middleContainer
+		Polymer.Base.transform("translate3d(0," + yRatio * 100 + "%,0)",
+			middleContainer);
 
-    // Scale bottomContainer and bottom sub title to nothing and back
-    Polymer.Base.transform('scale(' + scaleBottom + ') translateZ(0)',
-                           bottomContainer);
+		// Scale bottomContainer and bottom sub title to nothing and back
+		Polymer.Base.transform("scale(" + scaleBottom + ") translateZ(0)",
+			bottomContainer);
 
-    // Scale middleContainer appName
-    Polymer.Base.transform('scale(' + scaleMiddle + ') translateZ(0)', appName);
-  });
+		// Scale middleContainer appName
+		Polymer.Base.transform("scale(" + scaleMiddle + ") translateZ(0)", appName);
+	});
 
-  // Scroll page to top and expand header
-  app.scrollPageToTop = function() { app.$.headerPanelMain.scrollToTop(true); };
+	// Scroll page to top and expand header
+	app.scrollPageToTop = function () { app.$.headerPanelMain.scrollToTop(true); };
 
 })(document);
--- app/scripts/sockets.js
+++ app/scripts/sockets.js
@@ -1,54 +1,53 @@
 window.tweetSocket = (function (document) {
-    "use strict";
-    var socket = io();
-    var countStream = Rx.Observable.create();
-    var mySocket = {
-        tweetStream: function () {
-            var streamSource = Rx.Observable.create(function (observer) {
-                socket.on("tweet", function (tweet) {
-                    observer.onNext(tweet);
-                });
-            });
-            return streamSource.pausable();
-        },
-        statusStream: Rx.Observable.create(function (observer) {
-            socket.on("trainingStatus", function (status) {
-                observer.onNext({
-                    training: status.status === "RUNNING",
-                    message: status.message
-                });
-            });
-        }),
-        countStream: Rx.Observable.create(function (observer) {
-            socket.on("counts", function (counts) {
-                observer.onNext(counts);
-            });
-        }),
-        trackingStream: Rx.Observable.create(function (observer) {
-            socket.on("tracking", function (term) {
-                observer.onNext(term);
-            });
-        }),
-        requestTweets: function (afterTweetId, limit) {
-            return Rx.Observable.create(function (observer) {
-                socket
-                    .emit("tweetRequest", {
-                    afterTweetId: afterTweetId,
-                    limit: limit
-                }, function (tweets) {
-                    observer.onNext(tweets);
-                });
-            });
-        },
-        classifyTweet: function (options) {
-            socket.emit("classify", {
-                tweetId: options.tweetId,
-                class: options.classification
-            });
-        },
-        sendCommand: function (commandName, options) {
-            socket.emit(commandName, options);
-        }
-    };
-    return mySocket;
+	"use strict";
+	var socket = io();
+	var mySocket = {
+		tweetStream: function () {
+			var streamSource = Rx.Observable.create(function (observer) {
+				socket.on("tweet", function (tweet) {
+					observer.onNext(tweet);
+				});
+			});
+			return streamSource.pausable();
+		},
+		statusStream: Rx.Observable.create(function (observer) {
+			socket.on("trainingStatus", function (status) {
+				observer.onNext({
+					training: status.status === "RUNNING",
+					message: status.message
+				});
+			});
+		}),
+		countStream: Rx.Observable.create(function (observer) {
+			socket.on("counts", function (counts) {
+				observer.onNext(counts);
+			});
+		}),
+		trackingStream: Rx.Observable.create(function (observer) {
+			socket.on("tracking", function (term) {
+				observer.onNext(term);
+			});
+		}),
+		requestTweets: function (afterTweetId, limit) {
+			return Rx.Observable.create(function (observer) {
+				socket
+					.emit("tweetRequest", {
+						afterTweetId: afterTweetId,
+						limit: limit
+					}, function (tweets) {
+						observer.onNext(tweets);
+					});
+			});
+		},
+		classifyTweet: function (options) {
+			socket.emit("classify", {
+				tweetId: options.tweetId,
+				class: options.classification
+			});
+		},
+		sendCommand: function (commandName, options) {
+			socket.emit(commandName, options);
+		}
+	};
+	return mySocket;
 })(document);
